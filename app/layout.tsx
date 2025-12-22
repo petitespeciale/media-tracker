c4cf7ac34df572291e1c20af6ab9e43448dcbeff
carmille lim <carmillelim@carmilles-MacBook-Air.local>
2025-12-21 19:21:59 -0800

    feat: enable vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -88,7 +89,8 @@ export default function RootLayout({
             <BottomNav />
           </StoreProvider>
         </AuthProvider>
+        <Analytics />
       </body>
-    </html>
+    </html >
   );
 }
